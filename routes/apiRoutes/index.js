31b98257f5379b9b9be36b2bac65d3dd78812f3b
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-01-14 20:51:51 -0500

    votes routes done

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const db = require('../../db/connection');
 const router = express.Router();
 
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
 router.use(require('./voterRoutes'));
+router.use(require('./voteRoutes'));
+
+
+router.get('/votes', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id) AS count
+                 FROM votes
+                 LEFT JOIN candidates ON votes.candidate_id = candidates.id
+                 LEFT JOIN parties ON candidates.party_id = parties.id
+                 GROUP BY candidate_id ORDER BY count DESC;`;
+
+    const params = [req.params];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
 
 module.exports = router;
